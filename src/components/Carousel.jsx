fdebab3af3e6618265bf4ebc9847a45966675b69
Franco Rampazzo <franco.rodrigo.rampazzo@gmail.com>
2023-12-29 16:44:20 -0300

    hot fix

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,17 +1,16 @@
 import { Carousel } from 'react-carousel-minimal'
 
 const CarouselComponent = ({ images }) => {
-
   const data = images.map((item) => {
     return {
       image: item.img,
-
     }
   })
 
   return (
     <Carousel
       data={data}
+      width="100%"
       height="100%"
       slideNumber={false}
       automatic={false}
